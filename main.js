ccebe8091f4495fc6e5a26870a1633f655cb44c1
Adarsh Kumar <120160355+jinx-vi-0@users.noreply.github.com>
2023-11-23 22:12:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+
+var typed = new Typed(".auto-typed", {
+  strings: ["Web Designer.", "Problem Solver.", "Frontend Developer."],
+  typeSpeed: 80,
+  backSpeed: 20,
+  loop: true,
+});
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+/* -----------google sheet----------*/
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwkDTb98pF8ivB1cCHWzyOU-g82clcbWYuWHJw2MnKj8pGSuFI2oljbkC1h0hXGd8M/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent succesfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
